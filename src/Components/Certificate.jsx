ae967a9e4fd5af4e46560f245a7b3995754f1703
Michael Onyeabo <michyking1@gmail.com>
2023-03-06 09:10:43 +0100

    contact added

--- src/Components/Certificate.jsx
+++ src/Components/Certificate.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
 
-function certificate() {
+// UseEffect
+import { useEffect } from "react";
+
+// AOS
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+import cert from "../assets/certificate.png";
+
+function Certificate() {
+
+  useEffect(() => {
+    AOS.init({
+      mirror: true,
+      duration: 1500,
+      easing: "ease-in-out",
+      once: false,
+    });
+  }, []);
+
   return (
-    <div className='section' id='certificate'>certificate</div>
-  )
+    <section className="section bg-sitebg py-10" data-aos="fade-up">
+      <div className="container mx-auto">
+        <div>
+          <h1 className="h2 text-center mb-11 lg:mb-24">certification</h1>
+          <div className="flex flex-col-reverse lg:flex-row items-center justify-between gap-x-8 gap-y-7">
+            <p>
+              Professionally certified by Techathon Mentorship and Open Source
+              program. See official certificate <a href="http://michyking.com/wp-content/uploads/2023/03/TECHATHON_ONYEABO_MICHAEL_CHUKWUEBUKA_TM-ONMI24_CERTIFICATE.pdf"  className="text-primary">here</a> 
+            </p>
+            <img src={cert} alt="" className="rounded-md lg:max-w-xl" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default certificate
+export default Certificate;
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,9 +1,120 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+
+// icons
+import { MdEmail } from "react-icons/md";
+import { BsMessenger } from "react-icons/bs";
+
+// AOS
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+// emailjs 
+import emailjs from '@emailjs/browser';
+
 
 function Contact() {
+  useEffect(() => {
+    AOS.init({
+      mirror: true,
+      duration: 1500,
+      easing: "ease-in-out",
+      once: false,
+    });
+  }, []);
+
+  const form = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_hnx5mjd', 'template_o597dnn', form.current, '1Hnpq7tbrb48p7VS4')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset 
+  };
+
   return (
-    <div className='section' id='contact'>Contact</div>
-  )
+    <section className="my-16" id="contact">
+      <div className="container mx-auto">
+        <div className="" data-aos="fade-up">
+          <h1 className="h2 mb-11 lg:mb-24 text-center">Contact info</h1>
+          <div className="flex items-center justify-between flex-col lg:flex-row">
+            {/* social media links */}
+            <div className="flex flex-col items-center justify-center flex-1">
+              {/* email */}
+              <div
+                className="bg-sitebg rounded-md gap-y-2 py-7 px-4 flex flex-col 
+                justify-center items-center w-[300px] text-sm hover:bg-site hover:border
+                 hover:border-primary transition-all mb-7"
+              >
+                <MdEmail className="text-primary font text-[1.5rem] mb-3" />
+                <p>Email</p>
+                <p>onyeabomichael00@gmail.com</p>
+                <a
+                  href="mailto:onyeabomichael00@gmail.com"
+                  className="text-primary mt-3"
+                >
+                  Send a message
+                </a>
+              </div>
+              {/* messenger */}
+              <div
+                className="bg-sitebg rounded-md gap-y-2 py-7 px-4 flex flex-col 
+                justify-center items-center w-[300px] text-sm hover:bg-site hover:border
+                 hover:border-primary transition-all mb-7"
+              >
+                <BsMessenger className="text-primary font text-[1.5rem] mb-3" />
+                <p>Messenger</p>
+                <p>Michael Onyeabo</p>
+                <a
+                  href="https://m.me/michael.onyeabo.161"
+                  className="text-primary mt-3"
+                >
+                  Send a message
+                </a>
+              </div>
+            </div>
+            {/* form */}
+            <div className="flex justify-center items-center">
+              <form
+                onSubmit={submitHandler}
+                autoComplete="off"
+                ref={form}
+                action=""
+                className="flex flex-1 flex-col items-center justify-center gap-y-7
+                w-[250px] lg:w-[500px]"
+              >
+                <input
+                  type="text"
+                  placeholder="Your Name"
+                  className="form"
+                  name="user_name"
+                />
+                <input
+                  type="email"
+                  placeholder="Your email"
+                  className="form"
+                  name="user_email"
+                />
+                <textarea
+                  id=""
+                  cols="50"
+                  rows="5"
+                  className="form "
+                  placeholder="Your Message"
+                  name='message'
+                ></textarea>
+                <button className="btn btn-sm">Send message</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
