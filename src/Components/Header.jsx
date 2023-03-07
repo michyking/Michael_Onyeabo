74a6d2ec4ca6b0b81052b406f3dba0d183629082
Michael Onyeabo <michyking1@gmail.com>
2023-03-07 07:37:34 +0100

    resumed added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,9 @@ function Header() {
   return (
     <header className=' my-11 lg:my-8 md:py-8 lg:py-0' id='head'>
         <div className="container mx-auto flex items-center justify-end">
-          <button className="btn btn-sm text-[15px]">Online Resume</button>
+          <a href="http://michyking.com/wp-content/uploads/2023/03/michy-portfolio.pdf">
+          <button className="btn btn-sm text-[15px]">See my Resume</button>
+          </a>
         </div>
     </header>
   )
